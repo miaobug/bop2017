d9a2c45cbf71f7d3dc128080f3229e9c05155fa4
Miao Rui <1200012426@pku.edu.cn>
2017-08-14 19:37:35 +0800

    init commit

---
+++ src/components/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <img class="logo" src="../assets/logo.png" id="logo">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+    }
+  }
+}
+
+const height = document.documentElement.clientHeight;
+document.getElementById()
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .container {
+    text-align: center;
+    margin: 10px;
+    border: 1px solid #eee;
+  }
+  #logo {
+    width: 200px;
+    margin-top: 150px;
+  }
+  h1 {
+    color: darkred;
+  }
+</style>
