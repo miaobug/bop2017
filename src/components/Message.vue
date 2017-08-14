060a7e62b4ce344c011184f3d76ec929596df564
sudowind <wfpkueecs@163.com>
2017-08-15 02:11:36 +0800

    完成聊天界面

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,35 +1,157 @@
 <template>
   <div id="container">
-    <iframe
-      id="bot"
-      src='https://webchat.botframework.com/embed/headachePKU?s=GGxpMuU8D6M.cwA.K0E.o7u1TfdccY0LIc9Gc_lz8gqQUvbnORywUt9J3WkTc_g'
-      style="width: 100%"
-    >
-    </iframe>
+    <!--<iframe-->
+      <!--id="bot"-->
+      <!--src='https://webchat.botframework.com/embed/headachePKU?s=GGxpMuU8D6M.cwA.K0E.o7u1TfdccY0LIc9Gc_lz8gqQUvbnORywUt9J3WkTc_g'-->
+      <!--style="width: 100%"-->
+    <!--&gt;-->
+    <!--</iframe>-->
+    <div id="messageBody">
+      <div class="header">聊天</div>
+      <div id="messageContainer">
+        <div id="messageWrapper">
+          <div v-for="item in messages">
+            <div class="msg bot-msg" v-if="item.from === 'bot'">{{item.content}}</div>
+            <div class="msg user-msg" v-if="item.from === 'user'">{{item.content}}</div>
+            <div style="clear: both;"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="messageBar">
+      <input v-model="message" type="text" @keyup.enter="sendMsg">
+      <div id="sendBtn" @click="sendMsg"><img width="40" height="40" src="../assets/send.png" alt=""></div>
+    </div>
   </div>
 </template>
 
 <script>
+  import Vue from 'vue'
+  import axios from 'axios'
+  import localStorage from 'vue-localstorage'
+  Vue.use(localStorage)
 export default {
   name: 'hello',
   mounted () {
+    this.id = this.$route.params.id;
+    this.loadMsg();
     const height = document.documentElement.clientHeight;
-    document.getElementById('bot').style.height = (height - 0) + 'px';
+    document.getElementById('messageContainer').style.height = (height - 100) + 'px';
+    document.getElementById('messageWrapper').style.height = (height - 100) + 'px';
+    this.containerHeight = height - 100;
+    this.$nextTick(() => {
+      document.getElementById('messageContainer').scrollTop = document.getElementById('messageWrapper').scrollHeight - this.containerHeight
+    });
   },
   methods: {
+    sendMsg: function () {
+//      console.log(this.messages);
+      if (this.message.length > 0) {
+        this.messages.push({
+          content: this.message,
+          from: 'user',
+          type: 'msg'
+        });
+//        console.log(document.getElementById('messageContainer').scrollTop);
+//        console.log(document.getElementById('messageWrapper').scrollHeight);
+//        console.log(this.containerHeight);
+        this.$localStorage.set('msg' + this.id, JSON.stringify(this.messages));
+        axios.get('/api/query/' + this.message).then((response) => {
+          console.log(response.data);
+          if (response.data.direction === 'answer') {
+            this.messages.push({
+              content: response.data.param,
+              from: 'bot',
+              type: 'msg'
+            });
+            this.$localStorage.set('msg' + this.id, JSON.stringify(this.messages));
+          } else if (response.data.direction === 'navigate') {
+            this.messages.push({
+              content: '点击前往' + response.data.param,
+              from: 'bot',
+              type: 'nav'
+            });
+          }
+          this.message = '';
+          this.$nextTick(() => {
+            document.getElementById('messageContainer').scrollTop = document.getElementById('messageWrapper').scrollHeight - this.containerHeight
+          });
+        });
+      }
+    },
+    loadMsg: function () {
+      let tmp = this.$localStorage.get('msg' + this.id);
+      if (tmp) {
+        this.messages = JSON.parse(tmp)
+        console.log(this.messages)
+      }
+    }
   },
   data () {
     return {
+      messages: [],
+      message: '',
+      containerHeight: 0,
+      id: ''
     }
   }
 }
-
-
 
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   #container {
-    text-align: center;
+    /*text-align: center;*/
+    padding: 0;
+  }
+  .header {
+    font-size: 1.5rem;
+    color: #ffffff;
+    width: 100%;
+    height: 50px;
+    padding: 0.3rem 0.8rem;
+    background: #36baec;
+  }
+  #messageContainer {
+    overflow: auto;
+    font-size: 1rem;
+    padding: 0 1rem;
+  }
+  #messageBar {
+    border-top: 1px solid #a3a3a3;
+    width: 100%;
+    height: 50px;
+    padding: 0;
+  }
+  #messageBar input {
+    float: left;
+    height: 48px;
+    font-size: 1.5rem;
+    padding: 0 0.5rem;
+    border: none;
+    border-right: 1px solid #a3a3a3;
+    width: 85%;
+  }
+  #sendBtn {
+    padding: 5px;
+    float: left;
+    width: 15%;
+    height: 48px;
+  }
+  .user-msg {
+    max-width: 70%;
+    float: right;
+    background: #36baec;
+    color: #ffffff;
+  }
+  .bot-msg {
+    max-width: 70%;
+    float: left;
+    background: #cbcbcb;
+    color: #000;
+  }
+  .msg {
+    padding: 0.2rem 0.5rem;
+    margin: 0.5rem 0;
   }
 </style>
